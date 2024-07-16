7085d7d811d351a7921797ff532931f51546cd16
sagardange2579 <sagardange2579@gmail.com>
2024-07-16 17:35:09 +0530

    Add User test cases By Sonali Automation Tester

---
+++ src/main/java/Tutorial1/Demo1.java
@@ -0,0 +1,11 @@
+package Tutorial1;
+
+public class Demo1 {
+	public static void main(String args[])
+	{
+	System.out.println("Add new User Test Cases Written by Sonali Automation Tester");
+
+}
+}
+
+
