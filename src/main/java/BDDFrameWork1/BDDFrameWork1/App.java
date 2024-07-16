6449290ac569279d9de91d99643961431ed83985
sagardange2579 <sagardange2579@gmail.com>
2024-07-16 16:00:38 +0530

    BDD FrameWork is created

---
+++ src/main/java/BDDFrameWork1/BDDFrameWork1/App.java
@@ -0,0 +1,13 @@
+package BDDFrameWork1.BDDFrameWork1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
