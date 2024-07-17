f3bb8e63276570e267d4e9587853fe18fbfef662
sagardange2579 <sagardange2579@gmail.com>
2024-07-17 09:22:08 +0530

    Search test cases written by Amit Automation Tester

---
+++ src/main/java/Tutorial1/Demo3.java
@@ -0,0 +1,9 @@
+package Tutorial1;
+
+public class Demo3 {
+	public static void main(String args[])
+	{
+		System.out.println("Search Uesr PIM Page test cases written by Amit Automation Tester");
+	}
+
+}
